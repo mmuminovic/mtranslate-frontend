ac19d26085e14b85d3203769c7bc745959ec5296
Muhamed Muminovic <muminovic.muhamed01@gmail.com>
2022-07-27 21:31:54 +0200

    Updates

---
+++ precache-manifest.6a722d420d1114b81024b4fb4375309f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22173b1b1c8cb5f5f09e71c8bebf9192",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d08d1f639c8ac4c425c1",
+    "url": "./static/css/2.1cace5d8.chunk.css"
+  },
+  {
+    "revision": "b6a390d188014439a132",
+    "url": "./static/css/main.a687f2b7.chunk.css"
+  },
+  {
+    "revision": "d08d1f639c8ac4c425c1",
+    "url": "./static/js/2.f87720a5.chunk.js"
+  },
+  {
+    "revision": "a52153bcc847b73a84a3dde72b2b9c2e",
+    "url": "./static/js/2.f87720a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a390d188014439a132",
+    "url": "./static/js/main.f2c22909.chunk.js"
+  },
+  {
+    "revision": "247194fdc4d3e7bae258",
+    "url": "./static/js/runtime-main.001aafb3.js"
+  }
+]);
